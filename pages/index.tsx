f3e6e103cfbe3a5062b5a440756d39fcb978909b
ReddixT <reneottenburg@icloud.com>
2022-11-19 22:31:57 +0100

    feat: add landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,344 @@
-import { Header } from "../src/sections/Header";
-
-export default function Home() {
+import {
+    Box,
+    Flex,
+    Text,
+    IconButton,
+    Button,
+    Stack,
+    Collapse,
+    Icon,
+    Link,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    useColorModeValue,
+    useBreakpointValue,
+    useDisclosure,
+    VStack,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuDivider,
+    UnorderedList,
+    ListItem,
+    SimpleGrid
+} from '@chakra-ui/react';
+import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+} from '@chakra-ui/icons';
+import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
+import { ReactElement } from 'react';
+interface FeatureProps {
+    title: string;
+    icon: ReactElement;
+}
+const Feature = ({ title, icon }: FeatureProps) => {
     return (
-        <Header />
+        <Stack>
+            <Flex
+                w={16}
+                h={16}
+                align={'center'}
+                justify={'center'}
+                color={'white'}
+                rounded={'full'}
+                bg={'gray.100'}
+                mb={1}>
+                {icon}
+            </Flex>
+            <Text fontWeight={600}>{title}</Text>
+        </Stack>
+    );
+};
+
+export default function WithSubnavigation() {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Flex flexDirection="column">
+            <Box
+                position="fixed"
+                width="100%"
+                padding={"2%"}
+            >
+                <Flex
+                    backgroundColor={"rgba(0,0,0,0)"}
+                    color={useColorModeValue('gray.600', 'white')}
+                    minH={'60px'}
+                    py={{ base: 2 }}
+                    px={{ base: 4 }}
+                    align={'center'}>
+                    <Flex
+                        flex={{ base: 1, md: 'auto' }}
+                        ml={{ base: -2 }}
+                        display={{ base: 'flex', md: 'none' }}>
+                        <IconButton
+                            onClick={onToggle}
+                            icon={
+                                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+                            }
+                            variant={'ghost'}
+                            aria-label={'Toggle Navigation'}
+                        />
+                    </Flex>
+                    <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                        <Text
+                            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+                            fontSize="3xl">
+                            tectonica
+                        </Text>
+                    </Flex>
+                    <Stack
+                        flex={{ base: 1, md: 0 }}
+                        justify={'flex-end'}
+                        direction={'row'}
+                        spacing={6}>
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                minW={0}>
+                                <HamburgerIcon w={6} h={6} />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Referenzen</MenuItem>
+                                <MenuItem>Team</MenuItem>
+                                <MenuItem>Jobs</MenuItem>
+                                <MenuDivider />
+                                <MenuItem>Kontakt</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    </Stack>
+                </Flex>
+
+                <Collapse in={isOpen} animateOpacity>
+                    <MobileNav />
+                </Collapse>
+            </Box>
+            <Flex
+                w={'full'}
+                h={'50vh'}
+                backgroundImage={
+                    'url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'
+                }
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}
+            >
+
+                <VStack
+                    w={'full'}
+                    justify={'center'}
+                    px={useBreakpointValue({ base: 4, md: 8 })}
+                    bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+                    <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+                        <Text
+                            color={'white'}
+                            fontWeight={700}
+                            lineHeight={1.2}
+                            fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
+                            Lorem ipsum dolor sit amet consectetur adipiscing elit sed do
+                            eiusmod tempor
+                        </Text>
+                    </Stack>
+                </VStack>
+            </Flex>
+            <Flex
+                w={'full'}
+                h={'50vh'}
+                backgroundColor="black"
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}>
+                <VStack
+                    w={'full'}
+                    justify={'center'}
+                    px={useBreakpointValue({ base: 4, md: 8 })}
+                    bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+                    <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+                        <Text
+                            color={'white'}
+                            fontWeight={700}
+                            lineHeight={1.2}
+                            fontSize="xl">
+                            Vorstellung
+                        </Text>
+                    </Stack>
+                </VStack>
+            </Flex>
+            <Flex
+                w={'full'}
+                h={'50vh'}
+                backgroundColor="black"
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}>
+                <VStack
+                    w={'full'}
+                    justify={'center'}
+                    px={useBreakpointValue({ base: 4, md: 8 })}
+                    bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+                    <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+                        <Text
+                            color={'white'}
+                            fontWeight={700}
+                            lineHeight={1.2}
+                            fontSize="xl">
+                            Philosophie
+                        </Text>
+                    </Stack>
+                </VStack>
+            </Flex>
+            <Flex
+                w={'full'}
+                h={'50vh'}
+                backgroundColor="black"
+                backgroundSize={'cover'}
+                backgroundPosition={'center center'}>
+                <VStack
+                    w={'full'}
+                    justify={'center'}
+                    px={useBreakpointValue({ base: 4, md: 8 })}
+                    bgGradient={'linear(to-r, blackAlpha.600, transparent)'}>
+                    <Stack maxW={'2xl'} align={'flex-start'} spacing={6}>
+                        <Text
+                            color={'white'}
+                            fontWeight={700}
+                            lineHeight={1.2}
+                            fontSize="xl">
+                            Vision
+                        </Text>
+                        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+                            <Feature
+                                icon={<Icon as={FcAssistant} w={10} h={10} />}
+                                title={'Augenhöhe (Rhigetti Partner)'}
+                            />
+                            <Feature
+                                icon={<Icon as={FcDonate} w={10} h={10} />}
+                                title={'Denken auch ausserhalb des normalen Rahmens'}
+                            />
+                            <Feature
+                                icon={<Icon as={FcInTransit} w={10} h={10} />}
+                                title={'Den Ernst in die Sache investieren nicht in die Person'}
+                            />
+                        </SimpleGrid>
+                    </Stack>
+                </VStack>
+            </Flex>
+        </Flex >
     );
 }
+
+const MobileNav = () => {
+    return (
+        <Stack
+            bg={useColorModeValue('white', 'gray.800')}
+            p={4}
+            display={{ md: 'none' }}>
+            {NAV_ITEMS.map((navItem) => (
+                <MobileNavItem key={navItem.label} {...navItem} />
+            ))}
+        </Stack>
+    );
+};
+
+const MobileNavItem = ({ label, children, href }: NavItem) => {
+    const { isOpen, onToggle } = useDisclosure();
+
+    return (
+        <Stack spacing={4} onClick={children && onToggle}>
+            <Flex
+                py={2}
+                as={Link}
+                href={href ?? '#'}
+                justify={'space-between'}
+                align={'center'}
+                _hover={{
+                    textDecoration: 'none',
+                }}>
+                <Text
+                    fontWeight={600}
+                    color={useColorModeValue('gray.600', 'gray.200')}>
+                    {label}
+                </Text>
+                {children && (
+                    <Icon
+                        as={ChevronDownIcon}
+                        transition={'all .25s ease-in-out'}
+                        transform={isOpen ? 'rotate(180deg)' : ''}
+                        w={6}
+                        h={6}
+                    />
+                )}
+            </Flex>
+
+            <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+                <Stack
+                    mt={2}
+                    pl={4}
+                    borderLeft={1}
+                    borderStyle={'solid'}
+                    borderColor={useColorModeValue('gray.200', 'gray.700')}
+                    align={'start'}>
+                    {children &&
+                        children.map((child) => (
+                            <Link key={child.label} py={2} href={child.href}>
+                                {child.label}
+                            </Link>
+                        ))}
+                </Stack>
+            </Collapse>
+        </Stack>
+    );
+};
+
+interface NavItem {
+    label: string;
+    subLabel?: string;
+    children?: Array<NavItem>;
+    href?: string;
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: 'Inspiration',
+        children: [
+            {
+                label: 'Explore Design Work',
+                subLabel: 'Trending Design to inspire you',
+                href: '#',
+            },
+            {
+                label: 'New & Noteworthy',
+                subLabel: 'Up-and-coming Designers',
+                href: '#',
+            },
+        ],
+    },
+    {
+        label: 'Find Work',
+        children: [
+            {
+                label: 'Job Board',
+                subLabel: 'Find your dream design job',
+                href: '#',
+            },
+            {
+                label: 'Freelance Projects',
+                subLabel: 'An exclusive list for contract work',
+                href: '#',
+            },
+        ],
+    },
+    {
+        label: 'Learn Design',
+        href: '#',
+    },
+    {
+        label: 'Hire Designers',
+        href: '#',
+    },
+];
---
+++ pages/projects/index.tsx
@@ -0,0 +1,7 @@
+import { Header } from "../../src/sections/Header";
+
+export default function Home() {
+    return (
+        <Header />
+    );
+}
