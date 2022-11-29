de8fc34f8da54ecf3ac2df7aa4df1c3cbd249ce3
Luan <reneottenburg@icloud.com>
2022-11-29 13:55:57 +0100

    feat: add download button

--- src/sections/Carousel.tsx
+++ src/sections/Carousel.tsx
@@ -8,11 +8,13 @@ import {
     Text,
     Container,
     Button,
+    Link,
 } from '@chakra-ui/react';
 // Here we have used react-icons package for the icons
 import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 // And react-slick as our Carousel Lib
 import Slider from 'react-slick';
+import { Slide } from './Slide';
 
 // Settings for the slider
 const settings = {
@@ -20,9 +22,6 @@ const settings = {
     arrows: false,
     fade: true,
     infinite: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 5000,
     slidesToShow: 1,
     slidesToScroll: 1,
 };
@@ -36,32 +35,6 @@ export function Carousel() {
     // buttons as the screen size changes
     const top = useBreakpointValue({ base: '90%', md: '50%' });
     const side = useBreakpointValue({ base: '30%', md: '40px' });
-
-    // This list contains all the data for carousels
-    // This can be static or loaded from a server
-    const cards = [
-        {
-            title: 'Design Projects 1',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-        },
-        {
-            title: 'Design Projects 2',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
-        },
-        {
-            title: 'Design Projects 3',
-            text:
-                "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-        },
-    ];
 
     return (
         <Box
@@ -106,35 +79,27 @@ export function Carousel() {
             </IconButton>
             {/* Slider */}
             <Slider {...settings} ref={(slider) => setSlider(slider)}>
-                {cards.map((card, index) => (
-                    <Box
-                        key={index}
-                        height={'6xl'}
-                        position="relative"
-                        backgroundPosition="center"
-                        backgroundRepeat="no-repeat"
-                        backgroundSize="cover"
-                        backgroundImage={`url(${card.image})`}>
-                        {/* This is the block you need to change, to customize the caption */}
-                        <Container size="container.lg" height="full" position="relative">
-                            <Stack
-                                spacing={6}
-                                w={'full'}
-                                maxW={'lg'}
-                                position="absolute"
-                                top="50%"
-                                transform="translate(0, -50%)">
-                                <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                                    {card.title}
-                                </Heading>
-                                <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
-                                    {card.text}
-                                </Text>
-                                <Button>Download PDF</Button>
-                            </Stack>
-                        </Container>
-                    </Box>
-                ))}
+                <Slide
+                    index={0}
+                    title={"Heckenried Meggen"}
+                    text={"The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters."}
+                    image={'/img/2003_Heckenried_Meggen.jpg'}
+                    pdf={'2003_Heckenried_Meggen.pdf'}
+                />
+                <Slide
+                    index={1}
+                    title={"Design Projects 2"}
+                    text={"The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters."}
+                    image={'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80'}
+                    pdf={'2006_Wilstrasse_Dübendorf.pdf'}
+                />
+                <Slide
+                    index={2}
+                    title={"Design Projects 3"}
+                    text={"The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters."}
+                    image={"https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60"}
+                    pdf={'2008_VKU_Flughafen.pdf'}
+                />
             </Slider>
         </Box>
     );
