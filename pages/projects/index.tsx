c83c0be5837f86241bfd31a3e58ae224749a4e31
ReddixT <reneottenburg@icloud.com>
2022-12-21 13:21:49 +0100

    feat: add small image to each showcase project
    docs: add all pdfs

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Box,
     IconButton,
@@ -9,6 +9,8 @@ import {
     Container,
     Button,
     Flex,
+    Image,
+    Link,
 } from '@chakra-ui/react';
 import Slider from 'react-slick';
 import Router from 'next/router';
@@ -19,33 +21,36 @@ export default function Home() {
     const [slider, setSlider] = React.useState<Slider | null>(null);
     const top = useBreakpointValue({ base: '90%', md: '50%' });
     const side = useBreakpointValue({ base: '30%', md: '40px' });
+
     const projects = [
         {
             title: 'Heckenried Meggen',
             text:
                 "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1516796181074-bf453fbfa3e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-            pdf:
-                '2003_Heckenried_Meggen.pdf',
+            background:
+                'https://images.unsplash.com/photo-1526289034009-0240ddb68ce3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+            file:
+                '2003_Heckenried_Meggen',
+
+
         },
         {
             title: 'Wilstrasse Dübendorf',
             text:
                 "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1438183972690-6d4658e3290e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80',
-            pdf:
-                '2006_Wilstrasse_Dübendorf.pdf',
+            background:
+                'https://images.unsplash.com/photo-1510507024924-fc3847d49ae2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            file:
+                '2006_Wilstrasse_Dübendorf',
         },
         {
             title: 'VKU Flughafen',
             text:
                 "The project board is an exclusive resource for contract work. It's perfect for freelancers, agencies, and moonlighters.",
-            image:
-                'https://images.unsplash.com/photo-1507237998874-b4d52d1dd655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-            pdf:
-                '2008_VKU_Flughafen.pdf',
+            background:
+                'https://images.unsplash.com/photo-1529607697227-d616efef768f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            file:
+                '2008_VKU_Flughafen',
         },
     ];
     return (
@@ -93,7 +98,7 @@ export default function Home() {
                     dots={true}
                     arrows={true}
                     infinite={true}
-                    autoplay={true}
+                    autoplay={false}
                     speed={500}
                     autoplaySpeed={5000}
                     slidesToShow={1}
@@ -108,28 +113,65 @@ export default function Home() {
                             backgroundPosition="center"
                             backgroundRepeat="no-repeat"
                             backgroundSize="cover"
-                            backgroundImage={`url(${project.image})`}>
-                            <Container size="container.lg" height="600px" position="relative">
+                            backgroundImage={`url(${project.background})`}
+                        >
+                            <Flex justify="center">
                                 <Stack key={index}
-                                    spacing={6}
-                                    w={'full'}
-                                    maxW={'lg'}
+                                    w={'50%'}
                                     position="absolute"
                                     top="50%"
-                                    transform="translate(0, -50%)">
-                                    <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
-                                        {project.title}
-                                    </Heading>
-                                    <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
-                                        {project.text}
-                                    </Text>
-                                    <Button onClick={() => { Router.push(`/pdf/${project.pdf}`) }}>
-                                        Download PDF
-                                    </Button>
+                                    transform="translate(0, -70%)"
+                                >
+                                    <Flex width={"100%"}>
+                                        <Flex flexDir={"column"} mr="2%">
+                                            <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                                                {project.title}
+                                            </Heading>
+                                            <Text fontSize={{ base: 'md', lg: 'lg' }} color="GrayText">
+                                                {project.text}
+                                            </Text>
+                                            <Button onClick={() => { Router.push(`/pdf/${project.file}.pdf`) }}>
+                                                Download PDF
+                                            </Button>
+                                        </Flex>
+                                        <Flex width={"80%"}>
+                                            <Image src={`/img/${project.file}.jpg`} alt="Preview" />
+                                        </Flex>
+                                    </Flex>
                                 </Stack>
-                            </Container>
+                            </Flex>
                         </Box>
                     ))}
+                    <Box
+                        key={4}
+                        height={'6xl'}
+                        position="relative"
+                        backgroundPosition="center"
+                        backgroundRepeat="no-repeat"
+                        backgroundSize="cover"
+                    >
+                        <Flex justify="center">
+                            <Stack key={4}
+                                w={'50%'}
+                                position="absolute"
+                                top="50%"
+                                transform="translate(0, -70%)"
+                            >
+                                <Flex flexDir={"column"} mr="2%">
+                                    <Heading fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}>
+                                        Alle Projekte
+                                    </Heading>
+                                    <Box>
+                                        {/*
+                                            filenames.map((filename) => {
+                                                <Link href={filename}>{filename}</Link>
+                                            })
+                                        */}
+                                    </Box>
+                                </Flex>
+                            </Stack>
+                        </Flex>
+                    </Box>
                 </Slider>
             </Box>
         </Flex>
