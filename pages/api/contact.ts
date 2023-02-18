eac4c7ac49c5a202d9cde76467a4145fce612abc
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-18 15:35:27 +0100

    feat:send confirmation email

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,15 +5,25 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { email, subject, message } = req.body;
-    const msg = {
+    const msgTectonica = {
         to: process.env.COMPANY_CONTACT_EMAIL!,
         from: email,
+        replyTo: email,
         subject,
         text: message
     };
+    const msgCustomer = {
+        to: email,
+        from: process.env.COMPANY_NOREPLY_EMAIL!,
+        subject: `[Tectonica GmbH] Kontaktanfrage erhalten - ${subject}`,
+        text: `Wir haben Ihre Kontaktanfrage erhalten und werden auf Sie zukommen.\n\n
+            Betreff: "${subject}"\n
+            Mitteilung: "${message}"`
+    }
 
     try {
-        await sendgrid.send(msg);
+        await sendgrid.send(msgTectonica);
+        await sendgrid.send(msgCustomer);
         res.json({ message: 'email has been sent' });
     } catch (e) {
         res.status(500).json({ error: 'error sending email' });
