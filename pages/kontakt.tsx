6305bab4a4ec0cb198d3f850f799ec8bf3e9531d
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-18 17:07:07 +0100

    feat:add contact form

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,8 +1,32 @@
+import PageTitle from "@components/PageTitle";
+
 const Kontakt = () => {
+    const onSubmit = async (e: any) => {
+        e.preventDefault();
+        await fetch("/api/contact", {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({
+                email: e.target[0],
+                subject: e.target[1],
+                message: e.target[2]
+            })
+        });
+    };
+
     return (
-        <div>
-            Kontakt
-        </div>
+        <>
+            <PageTitle title={"tectonica | Kontakt"} />
+            <div>
+                Kontakt
+            </div>
+            <form>
+                <input type="text" id="email" name="email" /><br />
+                <input type="text" id="subject" name="subject" /><br />
+                <input type="text" id="message" name="message" /><br />
+                <input type="submit" value="Submit" onSubmit={onSubmit}/>
+            </form>
+        </>
     );
 }
 
