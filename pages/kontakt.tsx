dbdf0dd0d7426f053dfe88cae829ed029e8459fd
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-19 12:08:25 +0100

    feat:result screen for contact form

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -20,14 +20,49 @@ const Kontakt = () => {
             body: formJson
         });
         if (res.status == 200) {
-            setSuccess(true);
-            setTimeout(() => setSuccess(false), 10000);
             e.target.reset();
+            setSuccess(true);
         } else {
+            e.target.reset();
             setError(true);
-            setTimeout(() => setError(false), 10000);
         }
     };
+
+    let Form = () => {
+        return (
+            <form className="flex flex-col items-center w-full h-full gap-2 text-white" method='post' onSubmit={handleSubmit} id="contact-form">
+                <input type="email" placeholder="E-Mail Adrese" id="email" name="email" className="w-full max-w-xs input input-ghost " />
+                <input type="text" placeholder="Betreff" id="subject" name="subject" className="w-full max-w-xs input input-ghost " />
+                <textarea placeholder="Nachricht" id="message" name="message" className="w-full max-w-xs textarea input-ghost " />
+                <input type="submit" value="Submit" className="w-full max-w-xs text-gray-300 btn btn-outline" />
+            </form>
+        );
+    };
+
+    let ThankYou = () => {
+        return (
+            <div className="m-4 text-2xl text-center text-white grow">
+                Danke! Ihre Anfrage wird von uns bearbeitet.
+            </div>
+        );
+    };
+
+    let FormError = () => {
+        return (
+            <div className="m-4 text-2xl text-center text-white grow">
+                Ein unbekannter Fehler ist aufgetreten.
+                Bitte kontaktieren Sie uns direkt unter <a href="mailto:info@tectonica-bau.ch">info@tectonica-bau.ch</a>.
+            </div>
+        );
+    };
+
+    let FormThankYouError = Form;
+    if (success) {
+        FormThankYouError = ThankYou;
+    }
+    if (error) {
+        FormThankYouError = FormError;
+    }
 
     return (
         <>
@@ -44,28 +79,7 @@ const Kontakt = () => {
                         <div className="m-4 text-4xl font-bold text-center text-white grow">
                             Kontakt
                         </div>
-                        <form className="flex flex-col items-center w-full h-full gap-2 text-white" method='post' onSubmit={handleSubmit} id="contact-form">
-                            <input type="email" placeholder="E-Mail Adrese" id="email" name="email" className="w-full max-w-xs input input-ghost " />
-                            <input type="text" placeholder="Betreff" id="subject" name="subject" className="w-full max-w-xs input input-ghost " />
-                            <textarea placeholder="Nachricht" id="message" name="message" className="w-full max-w-xs textarea input-ghost " />
-                            <input type="submit" value="Submit" className="w-full max-w-xs text-gray-300 btn btn-outline" />
-                        </form>
-                        <div className="toast toast-center">
-                            {success &&
-                            <div className="alert alert-success">
-                                <div>
-                                    <span>Nachricht erhalten.</span>
-                                </div>
-                            </div>
-                            }
-                            {error &&
-                                <div className="alert alert-error">
-                                    <div>
-                                        <span>Unbekannter Fehler.</span>
-                                    </div>
-                                </div>
-                            }
-                        </div>
+                        <FormThankYouError />
                     </div>
                     <div id='map' className="w-full translate-y-1/2 bg-blue-400 shadow-lg translate-x-0sition-all sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 rounded-xl sm:hover:scale-105">
                         <GMap />
