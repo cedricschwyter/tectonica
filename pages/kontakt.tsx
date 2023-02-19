701efdc9da5a8436ea13dd45534a899334435e04
Cedric Schwyter <cedricschwyter@bluewin.ch>
2023-02-19 10:56:54 +0100

    feat:add contact form (#45)
    * feat:add contact form
    
    * feat: send email
    
    * feat: wip layout kontakt card
    
    * feat: add google maps lib
    
    * feat: add google maps
    
    * feat: make responsive
    
    * fix: use process env for api key
    
    * chore: clean up
    
    * fix: disable hover effect for mobile
    
    * fix: font color
    
    ---------
    
    Co-authored-by: theArgex <vargex@z2x.local>

--- pages/kontakt.tsx
+++ pages/kontakt.tsx
@@ -1,8 +1,51 @@
+import GMap from "@components/GMap";
+import PageTitle from "@components/PageTitle";
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+
 const Kontakt = () => {
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const formData = new FormData(form)
+        const formJson = JSON.stringify(Object.fromEntries(formData.entries()));
+        console.log(formJson);
+        await fetch("/api/contact", {
+            method: form.method,
+            headers: { "content-type": "application/json" },
+            body: formJson
+        });
+    };
+
     return (
-        <div>
-            Kontakt
-        </div>
+        <>
+            <PageTitle title={"tectonica | Kontakt"} />
+            <div className="grid w-full min-h-screen grid-cols-12 grid-rows-6 shadow-lg" style={{
+                backgroundImage: 'url(https://images.unsplash.com/photo-1553946550-4b8f3eea5451?ixlib=rb-4.0.3&ix)',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+            }}>
+
+                <div className="flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md">
+                    <div className="flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0">
+                        <div className="m-4 text-4xl font-bold text-center text-white grow">
+                            Kontakt
+                        </div>
+                        <form className="flex flex-col items-center w-full h-full gap-2 text-white" method='post' onSubmit={handleSubmit}>
+                            <input type="email" placeholder="E-Mail Adrese" id="email" name="email" className="w-full max-w-xs input input-ghost " />
+                            <input type="text" placeholder="Betreff" id="subject" name="subject" className="w-full max-w-xs input input-ghost " />
+                            <textarea placeholder="Nachricht" id="message" name="message" className="w-full max-w-xs textarea input-ghost " />
+                            <input type="submit" value="Submit" className="w-full max-w-xs text-gray-300 btn btn-outline" />
+                        </form>
+                    </div>
+                    <div id='map' className="w-full translate-y-1/2 bg-blue-400 shadow-lg translate-x-0sition-all sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 rounded-xl sm:hover:scale-105">
+                        <GMap />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
