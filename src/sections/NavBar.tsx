b5d060ba0e4c20670ca64fb8e0713fb6064de051
theArgex <vargex@z2x.local>
2023-02-19 20:33:56 +0100

    feat: new navbar design

--- src/sections/NavBar.tsx
+++ src/sections/NavBar.tsx
@@ -1,127 +1,141 @@
 import NextLink from 'next/link';
+import { FaChevronRight, FaChevronDown, FaBuilding, FaCamera, FaEye, FaUsers, FaBriefcase, FaCogs, FaNewspaper, FaEnvelope } from 'react-icons/fa';
 
 const navItems = [
     {
         name: 'Unternehmen',
         href: '/',
+        icon: <FaBuilding />,
         subNavItems: [
-            {
-                name: 'Portrait',
-                href: '/portrait',
-            },
+            // {
+            //     name: 'Portrait',
+            //     href: '/portrait',
+            //     icon: <FaCamera /> // needs better icon
+            // },
             {
                 name: 'Vision',
                 href: '/vision',
+                icon: <FaEye />
             },
             {
                 name: 'Team',
                 href: '/team',
+                icon: <FaUsers />
             },
             {
                 name: 'Jobs',
                 href: '/jobs',
+                icon: <FaBriefcase />
             },
         ],
     },
     {
         name: 'Dienstleistungen',
         href: '/dienstleistungen',
+        icon: <FaCogs />
     },
     {
         name: 'Aktuelles',
         href: '/aktuelles',
+        icon: <FaNewspaper />
     },
     {
         name: 'Kontakt',
         href: '/kontakt',
+        icon: <FaEnvelope />
     },
 ]
 
 export const NavBar = () => {
     return (
-        <div
-            className='fixed top-0 z-50 w-full pb-1 mr-3 shadow-lg bg-black/90 backdrop-blur-xl'
-        >
-            <div className='flex px-4 py-2 text-white align-center'>
-                <div className='flex justify-start basis-1'>
-                    <NextLink
-                        className='p-1 text-3xl text-left rounded-sm text-accent'
-                        href={"/"}>
-                        tectonica
-                    </NextLink>
-                </div>
-                <div className='hidden w-full gap-2 text-white sm:flex basis-1/2'>
-                    {
-                        navItems.map((item, index) => {
-                            return (
-                                <NextLink
-                                    className='w-full p-2 mx-2 font-semibold text-center transition-all rounded hover:shadow-md hover:bg-white/30 hover:scale-105 active:shadow-inner active:scale-95'
-                                    key={index}
-                                    href={item.href}
-                                    passHref
-                                >
-                                    {item.name}
-                                </NextLink>
-                            )
-                        })
-                    }
-                    {/* TODO: Dropdown for Unternehmen
-                    {
-                        navItems[0].subNavItems
-                            .map((item, index) => {
+        <div className="sticky top-0 z-50 w-auto sm:mx-2 sm:mb-3 sm:top-2 bg-white/70 sm:rounded-xl navbar backdrop-blur-lg">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box">
+                        {
+                            navItems.map((item, index) => {
                                 return (
-                                    <NextLink
-                                        className='w-full p-3 font-semibold text-center rounded hover:shadow-md hover:underline active:shadow-inner'
-                                        key={index}
-                                        href={item.href}
-                                        passHref
-                                    >
-                                        {item.name}
-                                    </NextLink>
-                                )
-                            })
-                    } */}
-                    {/* <NextLink className='w-full p-2 font-semibold text-center transition-all rounded hover:shadow-md hover:bg-white/30 hover:scale-105 active:shadow-inner active:scale-95 ' href={'/projects'}>Projekte</NextLink>
-                    <NextLink className='w-full p-2 font-semibold text-center transition-all rounded hover:shadow-md hover:bg-white/30 hover:scale-105 active:shadow-inner active:scale-95 ' href={'/team'}>Team</NextLink>
-                    <NextLink className='w-full p-2 font-semibold text-center transition-all rounded hover:shadow-md hover:bg-white/30 hover:scale-105 active:shadow-inner active:scale-95 ' href={'/jobs'}>Jobs</NextLink>
-                    <NextLink className='w-full p-2 font-semibold text-center transition-all rounded hover:shadow-md hover:bg-white/30 hover:scale-105 active:shadow-inner active:scale-95 ' href={'/contact'}>Kontakt</NextLink> */}
-                </div>
-                <div id='stack-chakra'
-                    className='sm:!hidden block'
-                // flex={{ base: 1, md: 0 }}
-                // justify={'flex-end'}
-                // direction={'row'}
-                // spacing={6}
-                >
-                    <div>
-                        <button
-                            className=''
-                        // as={Button}
-                        // rounded={'full'}
-                        // variant={'link'}
-                        // cursor={'pointer'}
-                        // minW={0}
-                        >
-                            {/* <HamburgerIcon w={6} h={6} color='white' /> */}
-                        </button>
-                        <div className='!bg-slate-900 p-6 flex flex-col'>
-                            {
-                                navItems.map((item, index) => {
-                                    return (
+                                    <li key={index}>
                                         <NextLink
-                                            className='w-full p-3 font-semibold text-center rounded hover:shadow-md hover:underline active:shadow-inner'
-                                            key={index}
                                             href={item.href}
                                             passHref
                                         >
+                                            {item.icon}
                                             {item.name}
+                                            {item.subNavItems && <FaChevronRight />}
                                         </NextLink>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
+                                        {item.subNavItems && (
+                                            <ul className='p-2 bg-white dropdown text-base-content'>
+                                                {item.subNavItems.map((subItem, subIndex) => {
+                                                    return (
+                                                        <li key={subIndex}>
+                                                            <NextLink
+                                                                href={subItem.href}
+                                                                passHref
+                                                            >
+                                                                {subItem.icon}
+                                                                {subItem.name}
+                                                            </NextLink>
+                                                        </li>
+                                                    )
+                                                })
+                                                }
+                                            </ul>
+                                        )
+                                        }
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
                 </div>
+                <NextLink
+                    className='p-2 text-3xl rounded-lg text-accent'
+                    href={"/"}>
+                    tectonica
+                </NextLink>
+            </div>
+            <div className="hidden navbar-end md:flex">
+                <ul className="px-1 menu menu-horizontal flex-nowrap">
+                    {
+                        navItems.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <NextLink
+                                        href={item.href}
+                                        passHref
+                                    >
+                                        {item.icon}
+                                        {item.name}
+                                        {item.subNavItems && <FaChevronDown />}
+                                    </NextLink>
+                                    {item.subNavItems && (
+                                        <ul className='w-full p-2 bg-white dropdown text-base-content'>
+                                            {item.subNavItems.map((subItem, subIndex) => {
+                                                return (
+                                                    <li key={subIndex}>
+                                                        <NextLink
+                                                            href={subItem.href}
+                                                            passHref
+                                                        >
+                                                            {subItem.icon}
+                                                            {subItem.name}
+                                                        </NextLink>
+                                                    </li>
+                                                )
+                                            })
+                                            }
+                                        </ul>
+                                    )
+                                    }
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
         </div>
     )
