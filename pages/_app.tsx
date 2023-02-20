731d023ff445ffefbf3c9fda9a9b3c72a1a569dd
Argex <24879428+theArgex@users.noreply.github.com>
2023-02-20 06:46:19 +0100

    New Nav and Daisy theme (#50)
    * chore: delete old contact page
    
    * chore: get rid of chakra
    
    * chore: get rid of chakra
    
    * feat: tectonica color theme
    
    * feat: new navbar design
    
    ---------
    
    Co-authored-by: theArgex <vargex@z2x.local>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
 import Layout from "@components/Layout";
-import { NavBar } from "@sections/NavBar";
-import { AnimatePresence } from "framer-motion";
 import '../src/global.css';
 
 
 function MyApp({ Component, pageProps, router }) {
   return (
     <>
-      <Layout>
+      <Layout data-theme="tectonica">
         <Component className='w-screen h-screen mt-32 bg-white' {...pageProps} key={router.asPath} />
       </Layout>
     </>
