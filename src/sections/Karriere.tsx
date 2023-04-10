07073898df2f42db5b06922855abaa2c45cc9d09
Alex Grädel <24879428+zeitiv@users.noreply.github.com>
2023-04-10 18:41:10 +0200

    feat: karriere text

--- src/sections/Karriere.tsx
+++ src/sections/Karriere.tsx
@@ -5,26 +5,46 @@ interface KarriereProps {
 const Karriere = ({ }: KarriereProps) => {
     return (
         <>
-            <div id="karriere" className="grid w-full min-h-screen grid-cols-12 grid-rows-6 shadow-lg">
-
-                <div className="flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md">
-                    <div className="flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0">
-                        <div className="m-4 text-4xl font-bold text-center text-white grow">
+            <div id="karriere" className="flex m-2">
+                <div className="flex pb-24 pt-20 h-full w-full m-2 md:mx-[20%] shadow-lg rounded-xl bg-black/40 backdrop-blur-xl hover:shadow-teal-600 hover:shadow-2xl transition-all">
+                    <div className="p-3">
+                        <h1 className="mb-6 text-6xl font-bold text-white grow">
                             Karriere
+                        </h1>
+                        <div className="text-white text-2xl">
+                            <p className="hidden">
+                                Wir realisieren im Grossraum Zuerich anspruchsvolle Bauwerke
+                                fuer Industrie, oeffentliche Hand und Private.
+                            </p>
+                            Zur Ergaenzung unseres Teams suchen wir eine/n
+                            <span className="text-teal-500 font-bold">
+                                {" "}
+                                dipl. Bauleiter/In Hochbau{" "}
+                            </span>
+                            mit Pensum von 50 - 100%.
+                            <p className="mt-5">
+                                Wir bieten fortschrittliche Anstellungsbedingungen, mindestens 5
+                                Wochen Ferien und, wo moeglich, ein gewisses Pensum im
+                                Home-Office.
+                            </p>
+                            <p className="text-white mt-5 text-xl">
+                                Sie sind interessiert an einer selbstaendigen Taetigkeit in
+                                einem kleinen Team? Dann freuen wir uns auf Ihre vollstaendige
+                                Bewerbung per Mail an{" "}
+                                <a
+                                    className="text-teal-500 font-bold hover:text-2xl transition-all"
+                                    href="mailto:nicole.loetscher@tectonica-bau.ch"
+                                >
+                                    nicole.loetscher@tectonica-bau.ch
+                                </a>
+                                .
+                            </p>
                         </div>
-                        <div className="text-white">
-                            Wir realisieren im Grossraum Zuerich anspruchsvolle Bauwerke fuer Industrie, oeffentliche Hand und Private. Zur Ergaenzung unseres Teams suchen wir eine/n dipl. Bauleiter/In Hochbau mit Pensum von 50 - 100%. Wir bieten fortschrittliche Anstellungsbedingungen, mindestens 5 Wochen Ferien und, wo moeglich, ein gewisses Pensum im Home-Office.
-                        </div>
-                        <div className="text-white">
-                            Sie sind interessiert an einer selbstaendigen Taetigkeit in einem kleinen Team? Dann freuen wir uns auf Ihre vollstaendige Bewerbung per Mail an <a href="mailto:nicole.loetscher@tectonica-bau.ch">nicole.loetscher@tectonica-bau.ch</a>.
-                        </div>
-                    </div>
-                    <div id='map' className="w-full translate-y-1/2 bg-blue-400 shadow-lg translate-x-0sition-all sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 rounded-xl sm:hover:scale-105">
                     </div>
                 </div>
             </div>
         </>
     );
-}
+};
 
 export default Karriere;
--- src/sections/Team.tsx
+++ src/sections/Team.tsx
@@ -49,32 +49,6 @@ const Team = ({ }: TeamProps) => {
                     >
                         {row.map((element, index) => {
                             return (
-                                // <div
-                                //   key={element.name}
-                                //   classNameName="grid w-full min-h-[50%] grid-cols-12 grid-rows-6"
-                                // >
-                                //   <div classNameName="flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md">
-                                //     <div classNameName="flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0">
-                                //       <div classNameName="m-4 text-4xl font-bold text-center text-white grow">
-                                //         {element.name}
-                                //       </div>
-                                //       <div classNameName="text-white mb-4">{element.position}</div>
-                                //     </div>
-                                //     <div
-                                //       id="map"
-                                //       classNameName="w-full translate-y-1/2 bg-blue-400 shadow-lg translate-x-0sition-all sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 rounded-xl sm:hover:scale-105"
-                                //     >
-                                //       <NextLink href={element.picture} target="_blank">
-                                //         <Image
-                                //           classNameName="rounded-xl object-cover"
-                                //           src={element.picture}
-                                //           layout="fill"
-                                //           alt={`Bild ${element.name}`}
-                                //         />
-                                //       </NextLink>
-                                //     </div>
-                                //   </div>
-                                // </div>
                                 <div className="mb-24 mx-2" key={index}>
                                     <div className="rounded-lg hover:shadow-teal-600 hover:shadow-3xl hover:scale-105 transition-all shadow-lg h-full block bg-black/25 backdrop-blur-md text-white min-w-[370px] ">
                                         <div className="flex justify-center">
