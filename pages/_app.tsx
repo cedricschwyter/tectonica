06f581b56945540375946bfd148807854603f584
Cedric Schwyter <cedric@schwyter.io>
2023-08-26 15:19:12 +0200

    chore: adjust robots

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,6 @@ import '../src/global.css';
 function MyApp({ Component, pageProps, router }) {
     return (
         <>
-            <meta name="robots" content="noindex,nofollow" />
-
             <Layout data-theme="tectonica">
                 <Component className='mt-32 bg-white' {...pageProps} key={router.asPath} />
             </Layout>
