275a2cab02057e887ee66621c6b8e0acff09fe67
Cedric Schwyter <cedric@schwyter.io>
2026-06-15 19:52:30 +0200

    fix: layout shift during cycling
    Signed-off-by: Cedric Schwyter <cedric@schwyter.io>

--- pages/referenzen.tsx
+++ pages/referenzen.tsx
@@ -115,9 +115,9 @@ const Referenzen = () => {
     return (
         <>
             <PageTitle title={"tectonica | Referenzen"} />
-            <div className="grid w-full min-h-screen grid-cols-12 grid-rows-6 justify-center">
-                <div className="flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md">
-                    <div className={cardItems[index].referenceSheet ? "flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0" : "flex flex-col w-full m-1 sm:translate-y-0 translate-y-1/4 rounded-2xl sm:m-0"}>
+            <div className={`grid w-full min-h-screen grid-cols-12 grid-rows-6 justify-center`}>
+                <div className={`flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md`}>
+                    <div className={cardItems[index].referenceSheet ? `flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0 ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}` : `flex flex-col w-full m-1 sm:translate-y-0 translate-y-1/4 rounded-2xl sm:m-0 ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}`}>
                         <div className="m-4 text-4xl font-bold text-center text-white grow">
                             {cardItems[index].projectName}
                         </div>
@@ -130,8 +130,8 @@ const Referenzen = () => {
                             <div
                                 className="btn bg-transparent text-white hover:text-teal-600 hover:border-teal-600 hover:bg-transparent"
                                 onClick={() => {
-                                    setIndex((index - 1 + cardItems.length) % cardItems.length);
                                     setPdfReady(false);
+                                    setIndex((index - 1 + cardItems.length) % cardItems.length);
                                 }}
                             >
                                 <FaArrowLeft />
@@ -139,8 +139,8 @@ const Referenzen = () => {
                             <div
                                 className="btn bg-transparent text-white hover:text-teal-600 hover:border-teal-600 hover:bg-transparent"
                                 onClick={() => {
-                                    setIndex((index + 1) % cardItems.length);
                                     setPdfReady(false);
+                                    setIndex((index + 1) % cardItems.length);
                                 }}
                             >
                                 <FaArrowRight />
@@ -148,10 +148,10 @@ const Referenzen = () => {
                         </div>
                     </div>
                     {cardItems[index].referenceSheet && (
-                        <div className={`bg-gray-200 w-full translate-y-1/2 shadow-lg translate-x-0 sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 sm:hover:scale-105 transition-opacity duration-300 ${pdfReady ? "opacity-100" : "opacity-0"}`} >
+                        <div className={`w-full translate-y-1/2 shadow-lg translate-x-0 sm:w-full sm:translate-x-1/2 2xl:translate-x-1/4 sm:translate-y-0 h-2/5 sm:h-4/5 sm:hover:scale-105 ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}`} >
                             <Document
                                 file={cardItems[index].referenceSheet}
-                                loading={<div className={`transition-opacity duration-300 ${pdfReady ? "opacity-0" : "opacity-100"}`} />}
+                                loading={<div className="w-full h-2/5 sm:h-4/5" />}
                             >
                                 <Thumbnail pageIndex={0}
                                     onRenderSuccess={() => setPdfReady(true)}
