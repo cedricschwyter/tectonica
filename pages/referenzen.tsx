ebdd1a680cdac9238235da7de028634ad8834604
Cedric Schwyter <cedric@schwyter.io>
2026-06-15 19:57:57 +0200

    chore: fix flash
    Signed-off-by: Cedric Schwyter <cedric@schwyter.io>

--- pages/referenzen.tsx
+++ pages/referenzen.tsx
@@ -116,8 +116,8 @@ const Referenzen = () => {
         <>
             <PageTitle title={"tectonica | Referenzen"} />
             <div className={`grid w-full min-h-screen grid-cols-12 grid-rows-6 justify-center`}>
-                <div className={`flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md`}>
-                    <div className={cardItems[index].referenceSheet ? `flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0 ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}` : `flex flex-col w-full m-1 sm:translate-y-0 translate-y-1/4 rounded-2xl sm:m-0 ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}`}>
+                <div className={`flex flex-col items-center justify-end h-full grid-rows-6 row-span-5 row-start-1 shadow-lg sm:row-start-2 sm:col-span-8 col-span-full sm:col-start-2 sm:row-span-4 sm:flex-row lg:col-span-8 lg:col-start-2 2xl:col-span-6 2xl:col-start-4 rounded-xl bg-black/25 backdrop-blur-md ${pdfReady ? "transition-opacity duration-300 opacity-100" : "opacity-0"}`}>
+                    <div className={cardItems[index].referenceSheet ? `flex flex-col w-full m-1 sm:translate-x-1/3 sm:translate-y-0 translate-y-1/4 2xl:translate-x-[16.666667%] rounded-2xl sm:m-0` : `flex flex-col w-full m-1 sm:translate-y-0 translate-y-1/4 rounded-2xl sm:m-0`}>
                         <div className="m-4 text-4xl font-bold text-center text-white grow">
                             {cardItems[index].projectName}
                         </div>
